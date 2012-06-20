ccaad839c6f40e02a5b5ecaba385b9de5151c812
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-20 11:36:29 +0200

    SearchEngine & SearchResult classes added

--- search.rb
+++ search.rb
@@ -13,6 +13,54 @@ CONFIG = begin
 
 require_relative './query'
 
+class BUP7
+
+    class Book
+
+    end
+
+    class SearchResult
+        def initialize(http_resp)
+            @html = http_resp.body
+        end
+
+        def parse()
+
+        end
+
+        def to_html
+            @html
+        end
+    end
+
+    class SearchEngine
+        def SearchEngine::query(q)
+            uri = URI(CONFIG[:url] + q)
+            headers = CONFIG[:headers]
+
+            http = Net::HTTP.new(uri.host, uri.port)
+
+            req = Net::HTTP::Get.new(uri.request_uri)
+            headers.each do |k,v|
+                req.add_field(k.to_s, v.to_s)
+            end
+
+            resp = http.start do |h|
+                h.request(req)
+            end
+
+            SearchResult.new(resp)
+        end
+    end
+
+    class SearchQuery
+        def send()
+            SearchEngine::query(self.to_s)
+        end
+    end
+
+end
+
 # TODO
 #
 # add headers, like Referer, User-Agent, etc
